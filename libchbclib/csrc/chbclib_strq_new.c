129a2a12545799fde1da19f989f8cd4ec695ed6b
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2015-02-22 05:57:37 -0500

    Making progress in creating the formation of the library.

--- libchbclib/csrc/chbclib_strq_new.c
+++ libchbclib/csrc/chbclib_strq_new.c
@@ -3,23 +3,37 @@
 #include <chorebox.h>
 #include <stdbool.h>
 
-static void st_init ( void );
-static void st_meth_add ( void  *srf_aa, char *rg_a );
 
-static chbclib_strq_cl st_mainclass;
-static bool st_inited = false;
+static bool st_meth_add ( void  *srf_aa, char *rg_a );
+
+
+
+
+typedef struct st_privato {
+  chorebox_str_list *qfront;
+  chorebox_str_list **qback;
+} st_privato;
+
+// st_r_oba = type: chbclib_strq_obj
+#define st_r_oba (*((chbclib_strq_obh)srf_aa))
+// Private parts ...
+#define st_r_pr ((st_privato *)(st_r_oba->prv))
+
+static chbclib_strq_cl st_mainclass =
+{
+  st_meth_add // m_add
+};
 
 
 chbclib_strq_obj chbclib_strq_new ( int rg_a )
 {
   chbclib_strq_obj lc_ret;
   bool lc_noyet;
+  st_privato *lc_prv;
   
-  // Before we do anything else, we must be sure that the class has
-  // been set up. (Really --- I got to look up how to let C do this
-  // for me --- but in the mean time ...)
-  st_init();
   
+  // Now comes the part where we mallocate the memory for the main
+  // object.
   lc_noyet = true;
   if ( rg_a == 0 )
   {
@@ -38,6 +52,34 @@ chbclib_strq_obj chbclib_strq_new ( int rg_a )
   {
     lc_ret = (chbclib_strq_obj)chorebox_mlc(sizeof(chbclib_strq_obst));
   }
+  lc_ret->eract = rg_a;
+  
+  
+  
+  
+  // And we now will mallocate the private parts.
+  lc_noyet = true;
+  if ( rg_a == 1 )
+  {
+    lc_ret->prv = malloc(sizeof(st_privato));
+    if ( (lc_ret->prv) == NULL )
+    {
+      free(lc_ret);
+      return NULL;
+    }
+    lc_noyet = false;
+  }
+  if ( lc_noyet )
+  {
+    lc_ret->prv = chorebox_mlc(sizeof(st_privato));
+  }
+  
+  
+  // Now we will make the privates comprehensible - and set them up.
+  lc_prv = (st_privato *)(lc_ret->prv);
+  lc_prv->qfront = NULL;
+  lc_prv->qback = &(lc_prv->qfront);
+  
   
   
   // Now we assign the object to a class of matching type:
@@ -49,17 +91,64 @@ chbclib_strq_obj chbclib_strq_new ( int rg_a )
 }
 
 
-static void st_init ( void )
+static bool st_meth_add ( void  *srf_aa, char *rg_a )
 {
-  if ( st_inited ) { return; }
+  bool lc_noyet;
+  chorebox_str_list *lc_nuitm; // The new item added to the queue
   
-  st_mainclass.m_add = st_meth_add;
+  // Yes - it's a simple return-without-error if st_r_oba is NULL.
+  // We can't consult st_r_oba->eract because it doesn't exist if
+  // st_r_oba is NULL.
+  if ( st_r_oba == NULL ) return false;
   
-  st_inited = true;
-}
-
-static void st_meth_add ( void  *srf_aa, char *rg_a )
-{
+  
+  // Now we mallocate the basic node of the new item.
+  lc_noyet = true;
+  if ( st_r_oba->eract == 1 )
+  {
+    lc_nuitm = (chorebox_str_list *)malloc(sizeof(chorebox_str_list));
+    if ( lc_nuitm == NULL ) { return false; }
+    lc_noyet = false;
+  }
+  if ( lc_noyet )
+  {
+    lc_nuitm = (chorebox_str_list *)chorebox_mlc(sizeof(chorebox_str_list));
+    lc_noyet = false;
+  }
+  lc_nuitm->str = NULL;
+  
+  // If the provided string is null, we now attach it and are done.
+  if ( rg_a == NULL )
+  {
+    (*(st_r_pr->qback))->nex = lc_nuitm;
+    st_r_pr->qback = &(lc_nuitm->nex);
+    return true;
+  }
+  
+  // If we are still here, it is our task to copy the provided string
+  // and include it in the node.
+  lc_noyet = true;
+  if ( st_r_oba->eract == 1 )
+  {
+    if ( !(chorebox_apend_string_rsl(&(lc_nuitm->str),rg_a) ) )
+    {
+      free(lc_nuitm);
+      return false;
+    }
+    lc_noyet = false;
+  }
+  if ( lc_noyet )
+  {
+    chorebox_apend_string(&(lc_nuitm->str),rg_a);
+    lc_noyet = false;
+  }
+  
+  
+  // If we have gotten this far, we should just connect the node, and
+  // call the operation a success.
+  (*(st_r_pr->qback))->nex = lc_nuitm;
+  st_r_pr->qback = &(lc_nuitm->nex);
+  return true;
 }
 
 
--- libchbclib/inc/chbclib.h
+++ libchbclib/inc/chbclib.h
@@ -2,14 +2,21 @@
 #ifndef L__CHBCLIB__H
 #define L__CHBCLIB__H
 
+#include <stdbool.h>
+
 typedef struct chbclib_strq_cl {
-  void (*m_add) ( void  *srf_aa, char *rg_a );
+  bool (*m_add) ( void  *srf_aa, char *rg_a );
   // Adds a new string to the queue. Failure to do so is a
-  // fatal-error to the program.
+  // fatal-error to the program if the objects 'erat' value
+  // is 0. Other wise, the boolean return value will let
+  // the calling program know whether or not the operation
+  // was a success.
 } chbclib_strq_cl;
 
 typedef struct chbclib_strq_obst {
   chbclib_strq_cl *cls;
+  void *prv;
+  int eract;
 } chbclib_strq_obst;
 typedef chbclib_strq_obst *chbclib_strq_obj;
 typedef chbclib_strq_obj *chbclib_strq_obh;
